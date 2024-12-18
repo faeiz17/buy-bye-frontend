f161dc82334aea578a0b046b39439b016afb3065
Muhammad Faeiz Furqan <114372023+faeiz17@users.noreply.github.com>
2024-12-18 05:39:27 +0500

    Update userService.js

--- buybye/src/services/user/userService.js
+++ buybye/src/services/user/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://buy-bye-backend.vercel.app/";
 
 export const signUpServiceFunc = async (userData) => {
   try {
